67d009e186d071a76218aa90e6b2f938eb21e716
ParkSohyunee <sohyunee016@gmail.com>
2023-09-04 23:22:18 +0900

    feat: next-auth session type 재설정

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * user type 재정의 => session.user 타입
+   */
+  interface Session {
+    user: {
+      username: string;
+      name: string;
+      email: string;
+      image: string;
+    } & DefaultSession["user"];
+  }
+}
