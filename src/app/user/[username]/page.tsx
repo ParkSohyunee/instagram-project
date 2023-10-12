fd95d0e647da95fb5fb3aad5aba27dedfdbf574d
ParkSohyunee <sohyunee016@gmail.com>
2023-10-12 22:58:15 +0900

    feat: user page - 사용자 프로필 가져오기 로직 구현

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,3 +1,16 @@
-export default function UserPage() {
-  return <>user</>;
+import { getUserForProfile } from "@/service/sanity";
+
+interface Props {
+  params: {
+    username: string;
+  };
+}
+
+export default async function UserPage({ params: { username } }: Props) {
+  // 상단 - 사용자의 프로필 이미지와 정보(username, name, 숫자)
+  // 하단 - 3개의 탭(posts, liked, bookmarks)
+  const user = await getUserForProfile(username);
+  console.log(user);
+
+  return <></>;
 }
