1199a4c4c7bac1bfbee9afe10f2fcfde9850a7da
ParkSohyunee <sohyunee016@gmail.com>
2023-12-11 18:13:29 +0900

    Feat: (강의수강) session의 token을 이용해서 userId 생성 - next-auth의 jwt callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -21,15 +21,23 @@ export const authOptions: NextAuthOptions = {
       });
       return true;
     },
-    async session({ session }) {
+    async session({ session, token }) {
       const user = session.user;
       if (user) {
         session.user = {
           ...user,
           username: user.email?.split("@")[0],
+          id: token.id as string, // jwt로 생성한 id를 session의 id를 만들어서 넣어줌
         };
       }
       return session;
+    },
+    async jwt({ token, user }) {
+      // 좋아요 요청시 userId를 보내주기위해 session의 tokent을 이용해서 userId 생성
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
     },
   },
   pages: {
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -6,6 +6,7 @@ declare module "next-auth" {
    */
   interface Session {
     user: {
+      id: string;
       username: string;
       name: string;
       email: string;
